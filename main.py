af396af97e163783fb2e93bd6bcc460b43883733
ByteWaiser <bytewaiser@gmail.com>
2020-05-07 23:01:19 +0300

    Fixed some spellings, created data dictionary

--- main.py
+++ main.py
@@ -1,9 +1,58 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://zagorapi.yemek.com/search/recipe?query=*&Start=0&Rows=12"
+mainUrl = "https://zagorapi.yemek.com"
+url = r"/search/recipe?recipeCategory=Hamburger&recipeCategoryUrl=%2ftarif%2fhamburgerler%2f&Start=0&Rows=12"
 
+def clean(cat):
+	errors = {
+		"Vi̇deo": "Video",
+		"Yöresel tari̇fler": "Yöresel tarifler",
+		"Hamur i̇şi̇": "Hamur işi",
+		"Prati̇k yemek": "Pratik yemek",
+		"Şerbetli̇ tatli": "Şerbetli tatlı",
+		"Pi̇lav": "Pilav",
+		"Prati̇k sütlü tatli": "Pratik sütlü tatlı",
+		"Tatli kurabi̇ye": "Tatlı kurabiye",
+		"Kurabi̇ye": "Kurabiye",
+		"Pi̇zza": "Pizza",
+		"Bakli̇yat": "Bakliyat",
+		"Çi̇kolatali tatli": "Çikolatalı tatlı",
+		"Zeyti̇nyağli": "Zeytintağlı"
+	}
 
-req = requests.get(url).json()
-title = req["Data"]["Posts"][0]["TitleCustomized"]
-print(title)
+	try:
+		last = errors[cat]
+	except:
+		last = cat
+	
+	return last
+
+while url:
+	req = requests.get(mainUrl + url).json()
+	url = req["Pagination"]["NextMaxUrl"]
+	
+	try:
+		posts = req["Data"]["Posts"]
+	except:
+		continue
+
+	for i in posts:
+		title = i["TitleCustomized"]
+		ingredients = i["RecipeIngredient"]
+		instructions = i["RecipeInstruction"]
+		category = i["Label"].capitalize()
+
+		category = clean(category)
+
+		data = {
+			"title": title,
+			"ingredients": ingredients,
+			"instructions": instructions,
+			"category": category
+		}
+
+	
+
+	
+	
